6a4a2cfaf173e47fad6693eb75b09897911b80b5
Tom Martin <tom.martin1239@gmail.com>
2023-05-21 03:09:05 +0100

    Replace redirect with inject to replace Lithium logic and make it compatible

--- src/main/java/com/github/tatercertified/fabricautocrafter/mixin/MixinHopperBlockEntity_RecipeInsertion.java
+++ src/main/java/com/github/tatercertified/fabricautocrafter/mixin/MixinHopperBlockEntity_RecipeInsertion.java
@@ -1,40 +1,69 @@
 package com.github.tatercertified.fabricautocrafter.mixin;
 
 import com.github.tatercertified.fabricautocrafter.CraftingTableBlockEntity;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.HopperBlockEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.ShapedRecipe;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Arrays;
 
 @Mixin(HopperBlockEntity.class)
-public class MixinHopperBlockEntity_RecipeInsertion {
+public abstract class MixinHopperBlockEntity_RecipeInsertion {
     @Shadow
     private static ItemStack transfer(@Nullable Inventory from, Inventory to, ItemStack stack, int slot, @Nullable Direction side) {
         throw new IllegalStateException("Mixin failed to apply");
     }
 
+    @Shadow protected static boolean isInventoryFull(Inventory inventory, Direction direction) {
+        throw new IllegalStateException("Mixin failed to apply");
+    }
+
     /**
+     * This is an @Inject because it cancels the Lithium logic, making it compatible with this mod
+     *
      * @author NotStirred
      * @reason Replace recipe insertion logic for auto-crafters
      */
-    @Redirect(method = "insert", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/entity/HopperBlockEntity;transfer(Lnet/minecraft/inventory/Inventory;Lnet/minecraft/inventory/Inventory;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/math/Direction;)Lnet/minecraft/item/ItemStack;"))
-    private static ItemStack insert(Inventory from, Inventory to, ItemStack stack, Direction side) {
-        if (to instanceof CraftingTableBlockEntity) {
-            return transferAutoCrafter(from, (CraftingTableBlockEntity) to, stack, side);
-        } else {
-            return HopperBlockEntity.transfer(from, to, stack, side);
+    @SuppressWarnings("InvalidInjectorMethodSignature")
+    @Inject(method = "insert", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/util/math/Direction;getOpposite()Lnet/minecraft/util/math/Direction;"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private static void insert(World world, BlockPos pos, BlockState state, Inventory from, CallbackInfoReturnable<Boolean> cir, Inventory to, Direction side) {
+        if (isInventoryFull(to, side)) {
+            cir.setReturnValue(false);
+            return;
         }
+        for (int i = 0; i < from.size(); ++i) {
+            if (from.getStack(i).isEmpty()) continue;
+            ItemStack stack = from.getStack(i).copy();
+            ItemStack outStack;
+            if (to instanceof CraftingTableBlockEntity) {
+                outStack = transferAutoCrafter(from, (CraftingTableBlockEntity) to, from.removeStack(i, 1), side);
+            } else {
+                outStack = HopperBlockEntity.transfer(from, to, from.removeStack(i, 1), side);
+            }
+            if (outStack.isEmpty()) {
+                to.markDirty();
+                cir.setReturnValue(true);
+                return;
+            }
+            from.setStack(i, stack);
+        }
+        cir.setReturnValue(false);
     }
 
     private static ItemStack transferAutoCrafter(@Nullable Inventory from, CraftingTableBlockEntity to, ItemStack stack, @Nullable Direction side) {
